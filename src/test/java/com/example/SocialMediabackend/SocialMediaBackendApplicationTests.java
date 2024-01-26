04840c139131e8effd61ed7ffdcb6e236387e41b
abhinav <83538181+abhinavph21@users.noreply.github.com>
2024-01-26 12:38:23 +0530

    spring-setup

---
+++ src/test/java/com/example/SocialMediabackend/SocialMediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialMediabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
